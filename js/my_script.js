e7d4db4e6b17644ed334c6a82d6aecc049f7f0ee
Lorna Powell <powell.lap@gmail.com>
2019-11-13 11:57:11 +0000

    Changed table colour

--- js/my_script.js
+++ js/my_script.js
@@ -1,23 +1,4 @@
-$(document).ready(function() { //this is the start of the (document).ready part - do not delete or alter
-
-
-var newrow = [["Broccoli","Bulgaria","12","70","Veg"], ["Orange","Morocco","15","50","Fruit"]];
-var newrow2 = 	["Orange","Morocco","15","50","Fruit"];
-var newrow3 = ["Tangerine","Cyprus","12","25","Fruit"];
-
-
-for (i = 0; i < newrow[i].length; i++) {	
-	for (j = 0; j < 1; j++) {
-		console.log("I am a th - "+newrow[i][j]);
-	}	
-	
-
-	
-		for (j = 1; j < newrow.length; j++) {
-			console.log("I am a td - "+newrow[i][j]);
-		}
-}
-
-)};		
-
-//this code is not complete and dies not run, but it is a starter for 10
+$(document).ready(function() {
+$("tr:odd").addClass("odd"); 
+$("tr:even").addClass("even"); 
+});
