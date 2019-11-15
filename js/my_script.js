df68ca7388f53f95f49eda7a13b251c306da377e
Lorna Powell <powell.lap@gmail.com>
2019-11-15 16:26:56 +0000

    Code re selected and unselected rows being highlighted or not-highlighted

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-$("tr:odd").addClass("odd"); 
-$("tr:even").addClass("even"); 
+
+    $("tr:odd").addClass("odd"); 
+    $("tr:even").addClass("even"); 
+
+    $("th").click(function(){
+        //a bit like initialising values get rid of classes set by jQuery
+        $("td").removeClass("selected").removeClass("not-selected");
+        $(this).siblings().addClass("selected");     
+        $("td:not(.selected)").addClass("not-selected");
+     
+
+    });
 });
